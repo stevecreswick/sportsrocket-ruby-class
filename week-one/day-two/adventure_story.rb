5182119c4b3b154a3a26067b2eafb9387bf29f35
Steve Creswick <screswick@gmail.com>
2016-11-10 18:02:05 -0500

    Add days work

--- week-one/day-two/adventure_story.rb
+++ week-one/day-two/adventure_story.rb
@@ -1,22 +1,27 @@
 # Create Choose Your Own Adventure Story
 
 class Story
-  def initialize()
-    puts random_roll( 20 )
+  attr_accessor paragraphs
+  attr_accessor story_events
+
+  def initialize( options )
+    @paragraphs = []
+    @story_events = []
+
+    build_paragraphs( options[ :paragraphs ] )
+    build_challenges( options[ :story_events ] )
   end
 
-  def random_roll( num )
-    rand( 1..num )
-  end
-
-  def choice()
-
-    if random_roll( stat ) > hero.difficuly_check
-      # Successful next paragraph
-    else
-      # Failure in the story
+  def build_paragraphs( text_blocks )
+    text_blocks.each | paragraph |
+      @paragraph.push( StoryParagraph.new( paragraph ) )
     end
+  end
 
+  def build_challenges( text_blocks )
+    text_blocks.each | story_event |
+      @story_events.push( StoryEvent.new( story_event ) )
+    end
   end
 end
 
@@ -34,28 +39,122 @@ class Hero
     puts "Enter your name:"
     @name = gets
   end
+end
 
-  def choice_success( stat_check, value )
-    if @stats[ stat_check ] >= value
-      true
-    else
-      false
+class StoryEvent
+  attr_accessor :choices
+
+  def initialize( options )
+    @choices = []
+
+    if options.is_a? Array
+      options.each do | choice |
+        @choices.push( ChoiceOption.new( choice ) )
+      end
+    end
+  end
+
+  def print_choices
+    choices.each do | option |
+      puts option.message
     end
   end
 end
 
-class Choice
-  def initialize( message, stat_check, value )
-    @
-  end
+class ChoiceOption
+  attr_accessor :message
+  attr_accessor :stat_check
 
-  def choice_statement
-  end
-
-  def display_options
+  def initialize( options )
+    @message = options[ :message ]
+    @stat_check = StatCheck.new(
+      options[ :skill ],
+      options[ :value ]
+    )
   end
 end
 
+class StatCheck
+  attr_accessor :skill
+  attr_accessor :value
 
+  def initialize( skill, value )
+    @skill = skill
+    @value = value
+  end
+end
 
-aladdin = Story.new
+class StoryParagraph
+  attr_accessor :message
+
+  def initialize( message )
+    @message = message
+  end
+end
+
+story_paragraphs = [
+  'You walk alone in a dark wood.',
+  'You come upon a house in the woods made of Candy, yet someone inside eats children for sustenance.',
+  'The big bad wolf actually blue the house down...too much complaining, so the house collapsed.'
+];
+
+#
+# class ForkBuilder
+#
+#   def build_message
+#   end
+#
+#   def build_skill_check
+#   end
+#
+#   def build_value
+#   end
+# end
+
+story_fork_one = [
+  {
+    message: 'This is a story option 1.',
+    skill:  'strength',
+    value:  4
+  },
+  {
+    message: 'This is a story option 2.',
+    skill:  'speed',
+    value:  3
+  },
+  {
+    message: 'This is a story option 3.',
+    skill:  'durability',
+    value:  7
+  }
+];
+
+story_fork_two = [
+  {
+    message: 'This is a story option 1423423.',
+    skill:  'strength',
+    value:  4
+  },
+  {
+    message: 'This is a story option 21431234.',
+    skill:  'speed',
+    value:  3
+  },
+  {
+    message: 'This is a story option 234123423142134.',
+    skill:  'durability',
+    value:  7
+  }
+];
+
+# Story Forks, an array of option hashes
+
+story_event_one = StoryEvent.new( @story_fork_one )
+story_event_two = StoryEvent.new( @story_fork_two )
+
+story_options = {
+  paragraphs: story_paragraphs,
+  story_events: [ story_event_one, story_event_two ]
+}
+ puts story_options
+@story = Story.new( story_options )
